36a3c8a92017b620c2910401ce98b5e0249a0061
Amit Burstein <amit.burstein@gmail.com>
2014-04-26 00:30:11 -0700

    Instant search! sorta

--- public/js/collablists.js
+++ public/js/collablists.js
@@ -24,31 +24,30 @@ $(function() {
   }
 
   function showResults(results) {
-    var output = new Array();
-    if (result.status.code != 5) {
-      for each (song in results.songs) {
+    var output = [];
+    if (results.response.status.code != 5) {
+      $.each(results.response.songs, function(i, song) {
         var songInfo = {};
-        // Isolating song title and artist
         songInfo.title = song.title;
-        songInfo.artist = song.artist_id;
-        // Isolating album art and track id
-        for each (track in song.tracks) {
-          if (songInfo.album_art && track.release_image) {
+        songInfo.artist = song.artist_name;
+        $.each(song.tracks, function(j, track) {
+          if (!songInfo.album_art && track.release_image) {
             songInfo.album_art = track.release_image;
           }
-          if (songInfo.id && (song.id === 'rdio-US')) {
+          if (!songInfo.id && (track.catalog === 'rdio-US')) {
             songInfo.id = track.foreign_id;
           }
-        }
+        });
         output.push(songInfo);
-      }
+      });
     }
 
-    // do something with output
+    console.log(output);
 
+    // do something with output
     $('#results').empty();
-    for (var i = 0; i < sample.results.length; i++) {
-      var result = '<div class="result"><p class="result-song">' + sample.results[i].title + '</p><p class="result-artist">' + sample.results[i].artist + '</p></div>'
+    for (var i = 0; i < output.length; i++) {
+      var result = '<div class="result"><p class="result-song">' + output[i].title + '</p><p class="result-artist">' + sample.results[i].artist + '</p></div>'
       $('#results').append(result);
     }
   }
