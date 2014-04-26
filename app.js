066268bfc2c2c054b246ed7767e978c793881c97
Phillip Huang <phillippianist@gmail.com>
2014-04-25 23:10:23 -0700

    Added a route for playlists. It doesn't talk to the db yet.

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/:playlistURL', routes.servePlaylistURL);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,12 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.servePlaylistURL = function(req, res){
+	// check if the playlist URL is valid
+	if (req.params.playlistURL == "deadbeef")
+		res.send("Your playlist URL is : " + req.params.playlistURL);
+	else
+		// it's not valid, so we serve up a 404 not found page
+		res.render('notfound', { title: 'Express' });
+};
