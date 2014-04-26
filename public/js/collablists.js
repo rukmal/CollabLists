c5f0db1b548bc1eb47a5fd8454b993780396a0a6
Amit Burstein <amit.burstein@gmail.com>
2014-04-26 01:21:51 -0700

    clean shit

--- public/js/collablists.js
+++ public/js/collablists.js
@@ -30,38 +30,33 @@ $(function() {
 
   function search(data) {
     var echoNestURL = 'http://developer.echonest.com/api/v4/song/search?api_key=JBLMC5BKDIPYMLYD5&bucket=id:rdio-US&bucket=tracks&results=3&bucket=id:7digital-US&sort=song_hotttnesss-desc&title=';
-    data = data.replace(' ', '+');
-    $.get(echoNestURL + data, showResults);
-  }
-
-  function showResults(results) {
-    var output = [];
-    if (results.response.status.code != 5) {
-      $.each(results.response.songs, function(i, song) {
-        var songInfo = {};
-        songInfo.title = song.title;
-        songInfo.artist = song.artist_name;
-        $.each(song.tracks, function(j, track) {
-          if (!songInfo.album_art && track.release_image) {
-            songInfo.album_art = track.release_image;
+    $.get(echoNestURL + data, function(results) {
+      var output = [];
+      if (results.response.status.code === 0) {
+        $.each(results.response.songs, function(i, song) {
+          var songInfo = {};
+          songInfo.title = song.title;
+          songInfo.artist = song.artist_name;
+          $.each(song.tracks, function(j, track) {
+            if (!songInfo.album_art && track.release_image) {
+              songInfo.album_art = track.release_image;
+            }
+            if (!songInfo.id && (track.catalog === 'rdio-US')) {
+              songInfo.id = track.foreign_id;
+            }
+          });
+          if (!songInfo.album_art) {
+            songInfo.album_art = 'img/default-album.png';
           }
-          if (!songInfo.id && (track.catalog === 'rdio-US')) {
-            songInfo.id = track.foreign_id;
-          }
+          output.push(songInfo);
         });
-        if (!songInfo.album_art) {
-          songInfo.album_art = 'img/default-album.png';
-        }
-        output.push(songInfo);
-      });
-    }
+      }
 
-    // do something with output
-    $('#results').empty();
-    $.each(output, function(i, song) {
-      var result = '<div class="result"><img src="' + song.album_art + '" class="result-album-art"><div class="result-info"><p class="result-song">' + song.title + '</p><p class="result-artist">' + song.artist + '</p></div></div>'
-      // $('#results').append(result);
-      $(result).appendTo('#results').hide().fadeIn(200);
+      $('#results').empty();
+      $.each(output, function(i, song) {
+        var result = '<div class="result"><img src="' + song.album_art + '" class="result-album-art"><div class="result-info"><p class="result-song">' + song.title + '</p><p class="result-artist">' + song.artist + '</p></div></div>'
+        $(result).appendTo('#results').hide().fadeIn(200);
+      });
     });
   }
 });
